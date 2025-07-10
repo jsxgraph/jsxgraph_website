e1c0abff8614185469a37a11fdaa29e3b21eb874
Andreas Walter <andreas.walter@uni-bayreuth.de>
2025-07-10 14:10:39 +0200

    improve regexp for links

--- plugins/post_markdown.rb
+++ plugins/post_markdown.rb
@@ -3,22 +3,22 @@
 module Jekyll
   module PostMarkdown
     def replace_buttons(input)
-      input = input.gsub(/<a(.*)type="button"( colorclass="([^"]+)")?( class="([^"]+)")?/, '<a\1class="btn btn-\3 d-flex justify-content-center align-items-center \5"')
-      input = input.gsub(/<button(.*)type="button"( colorclass="([^"]+)")?( class="([^"]+)")?/, '<button\1type="button" class="btn btn-\3 d-flex justify-content-center align-items-center \5"')
+      input = input.gsub(/<a([^"]*)type="button"( colorclass="([^"]+)")?( class="([^"]+)")?/, '<a\1class="btn btn-\3 d-flex justify-content-center align-items-center \5"')
+      input = input.gsub(/<button([^"]*)type="button"( colorclass="([^"]+)")?( class="([^"]+)")?/, '<button\1type="button" class="btn btn-\3 d-flex justify-content-center align-items-center \5"')
       input = input.gsub('btn--gradient', 'btn-primary-gradient')
     end
 
     def replace_links(input)
       # Add target="_blank" to external links
-      input = input.gsub(/<a(.*)href="http([^"]+)"([^>]*)>(.*)<\/a>/, '<a\1href="http\2" target="_blank"\3>\4<i class="fa-solid fa-arrow-up-right-from-square icon-external-link"></i></a>');
+      input = input.gsub(/<a([^"]*)href="http([^"]+)"([^>]*)>([^<]*)<\/a>/, '<a\1href="http\2" target="_blank"\3>\4<i class="fa-solid fa-arrow-up-right-from-square icon-external-link"></i></a>');
       # Add icon to YouTube and mundo links
-      input = input.gsub(/<a(.*)href="([^"]*)(youtube|mundo\.schule)([^"]*)"([^>]*)>(.*)<\/a>/, '<a\1href="\2\3\4" target="_blank"\5><i class="fa-brands fa-fw fa-lg fa-youtube me-1"></i>\6</a>');
+      input = input.gsub(/<a([^"]*)href="([^"]*)(youtube|mundo\.schule)([^"]*)"([^>]*)>/, '<a\1href="\2\3\4" target="_blank"\5><i class="fa-brands fa-fw fa-lg fa-youtube me-1"></i>');
       # Add target="_blank" and icon to files
-      input = input.gsub(/<a(.*)href="([^"]+)\.(pdf)"([^>]*)>(.*)<\/a>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-pdf me-1"></i>\5</a>');
-      input = input.gsub(/<a(.*)href="([^"]+)\.(gif|jpg|jpeg|png|svg)"([^>]*)>(.*)<\/a>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-image me-1"></i>\5</a>');
-      input = input.gsub(/<a(.*)href="([^"]+)\.(zip)"([^>]*)>(.*)<\/a>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-zipper me-1"></i>\5</a>');
-      input = input.gsub(/<a(.*)href="([^"]+)\.(ttf|woff)"([^>]*)>(.*)<\/a>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-word me-1"></i>\5</a>');
-      input = input.gsub(/<a(.*)href="([^"]+)\.(jc|js|sketcho|css|map)"([^>]*)>(.*)<\/a>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-code me-1"></i>\5</a>');
+      input = input.gsub(/<a([^"]*)href="([^"]+)\.(pdf)"([^>]*)>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-pdf me-1"></i>');
+      input = input.gsub(/<a([^"]*)href="([^"]+)\.(gif|jpg|jpeg|png|svg)"([^>]*)>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-image me-1"></i>');
+      input = input.gsub(/<a([^"]*)href="([^"]+)\.(zip)"([^>]*)>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-zipper me-1"></i>');
+      input = input.gsub(/<a([^"]*)href="([^"]+)\.(ttf|woff)"([^>]*)>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-word me-1"></i>');
+      input = input.gsub(/<a([^"]*)href="([^"]+)\.(jc|js|sketcho|css|map)"([^>]*)>/, '<a\1href="\2.\3" target="_blank"\4><i class="fa-solid fa-fw fa-lg fa-file-code me-1"></i>');
     end
 
     def replace_tables(input)
